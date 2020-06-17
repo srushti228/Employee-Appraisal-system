70ac822d8a16c3a7a8cd2407dcfb4e768e5d5b34
srushti <srushtihp3@gmail.com>
2020-06-17 22:50:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Employee Appraisal Management System
+Made using AngularJS, Express, Node and database as MongoDB
+
+Performs CRUD operations.
+
+To Run:
+1. npm install
+2. sudo npm install -g express
+3. sudo npm install -g body-parser
+4. sudo npm install -g mongoose
+5. In another terminal run mongod.
+6. node server.js
+
+After these steps open http://localhost:3000. 
---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/employees');
+var Employee = mongoose.model('Employee', mongoose.Schema({
+	name:String,
+	dept:String,
+	age:String,
+	status:String,
+	contact:String,
+	salary:String,
+	remarks:String
+}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+
+app.get('/api/employees', function(req, res){
+	Employee.find(function(err, employees){
+		if(err)
+			res.send(err);
+		res.json(employees);
+	});
+});
+
+app.get('/api/employees/:id', function(req, res){
+	Employee.findOne({_id:req.params.id}, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.post('/api/employees', function(req, res){
+	Employee.create( req.body, function(err, employees){
+		if(err)
+			res.send(err);
+		res.json(employees);
+	});
+});
+
+app.delete('/api/employees/:id', function(req, res){
+	Employee.findOneAndRemove({_id:req.params.id}, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.put('/api/employees/:id', function(req, res){
+	var query = {
+		name:req.body.name,
+		age:req.body.age,
+		dept:req.body.dept,
+		status:req.body.status,
+		contact:req.body.contact,
+		salary:req.body.salary,
+		remarks:req.body.remarks
+	};
+	Employee.findOneAndUpdate({_id:req.params.id}, query, function(err, employee){
+		if(err)
+			res.send(err);
+		res.json(employee);
+	});
+});
+app.listen(3000, function(){
+	console.log('server is running on port 3000..');
+});
